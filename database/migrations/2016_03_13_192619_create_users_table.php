9eee9175d7b5bebeac561e25189a435a0410faee
supaheckafresh <neilstrain@gmail.com>
2016-03-13 13:37:29 -0700

    Add sqlite db and a couple of user records

--- database/migrations/2016_03_13_192619_create_users_table.php
+++ database/migrations/2016_03_13_192619_create_users_table.php
@@ -14,10 +14,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->required();
-            $table->string('email')->required()->unique();
-            $table->string('phone')->required();
-            $table->string('password')->required()->unique();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('password');
             $table->string('img_url');
             $table->timestamps();
         });
