f7031dee99f2e823eb36d12e37d7456c91a2c189
supaheckafresh <neilstrain@gmail.com>
2016-03-13 13:48:41 -0700

    Add bower dependencies

--- resources/views/index.php
+++ resources/views/index.php
@@ -3,9 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <title>Contacts App</title>
+
+    <link rel="stylesheet" href="/bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/bower_components/font-awesome/css/font-awesome.min.css">
+
 </head>
 <body>
     <h1>Contacts</h1>
+
+
+
+    <script src="/bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="/bower_components/lodash/dist/lodash.core.min.js"></script>
+    <script src="/bower_components/angular/angular.min.js"></script>
+    <script src="/bower_components/angular-bootstrap/ui-bootstrap.min.js"></script>
 
 </body>
 </html>
