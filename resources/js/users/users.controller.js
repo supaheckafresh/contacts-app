99cd34db59beb3ef3ff7a800f703fe1456be6fc5
supaheckafresh <neilstrain@gmail.com>
2016-03-13 16:39:27 -0700

    Add gulp

---
+++ resources/js/users/users.controller.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Neil Strain on 3/13/2016.
+ */
+
+(function () {
+
+    'use strict';
+
+    angular.module('contactsApp')
+        .controller('ContactsController', function ($http) {
+            var vm = this;
+
+            $http.get('get-users')
+                .then(function successCb(res) {
+                    vm.users = res.data;
+                }, function errorCb() {
+                    alert('There was an error retrieving Users data');
+                });
+
+            vm.addUser = function () {
+
+            }
+        });
+
+}());
--- resources/views/index.php
+++ resources/views/index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <title>Contacts App</title>
 
-    <link rel="stylesheet" href="/css-deps/bootstrap.min.css">
-    <link rel="stylesheet" href="/css-deps/font-awesome.min.css">
+    <link rel="stylesheet" href="/css/deps.css">
+    <link rel="stylesheet" href="/css/styles.css">
 
 </head>
 <body>
@@ -29,12 +29,8 @@
     </div>
 
 
-
-    <script src="/js-deps/jquery.min.js"></script>
-    <script src="/js-deps/lodash.core.min.js"></script>
-    <script src="/js-deps/angular.min.js"></script>
-    <script src="/js-deps/ui-bootstrap.min.js"></script>
-    <script src="/js-deps/contactsapp.module.js"></script>
+    <script src="/js/deps.js"></script>
+    <script src="/js/app.js"></script>
 
 </body>
 </html>
