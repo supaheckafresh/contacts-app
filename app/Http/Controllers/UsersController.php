e5c630c0483031553191944b7eaf0e23f2e4098e
supaheckafresh <neilstrain@gmail.com>
2016-03-13 20:04:26 -0700

    Save new user to databse

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,11 +4,24 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Laravel\Lumen\Routing\Controller;
+use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    public function index ()
+    public function index()
     {
         return User::all();
+    }
+
+    public function addUser(Request $request)
+    {
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->phone = $request->phone;
+        $user->password = $request->password;
+        $user->img_url = $request->img_url;
+
+        $user->save();
     }
 }
